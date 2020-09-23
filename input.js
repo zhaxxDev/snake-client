596619be934ae690ae0cbb1970184f96e8cae42a
Z Xian <alex.xian@hotmail.com>
2020-09-23 20:13:19 +0000

    moved stuff round

---
+++ input.js
@@ -0,0 +1,20 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  //turns off 
+  stdin.on('data', (key) => {
+    process.stdout.write(key);
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  stdin.resume();
+  return stdin;
+}
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,24 +1,10 @@
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-
-  //turns off 
-  stdin.on('data', (key) => {
-    process.stdout.write(key);
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  stdin.resume();
-  return stdin;
-}
+const setupInput = require('./input')
 
 const connect = require('./client');
+
 console.log('Connecting ...');
+
 connect();
+
+
 setupInput()
