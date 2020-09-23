d541ba2cdfcf3a08b7dbd9596bf025e311e703ab
Z Xian <alex.xian@hotmail.com>
2020-09-23 20:06:08 +0000

    added inpout detection

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  //turns off 
+  stdin.on('data', (key) => {
+    process.stdout.write(key);
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  stdin.resume();
+  return stdin;
+}
+
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+setupInput()
